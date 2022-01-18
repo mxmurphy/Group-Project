d08a88d75fd52d91d2a61482f479f7d349e5cff6
mikebarrett19 <97762854+mikebarrett19@users.noreply.github.com>
2022-01-18 13:48:25 -0600

    added the rest of the potential parameters

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     Authors: Brit, Julian, Michael
 
 */
-const fetchSearch = (event) => {
+const fetchSearch = () => {
     const checkboxes = document.getElementsByClassName("checkbox")
     const numBoxes = checkboxes.length();
     let searchParams = [];
@@ -14,14 +14,33 @@ const fetchSearch = (event) => {
             searchParams.push(currentCheckbox.innerText)
         }
     }
-    //turn it into fetch query format
+    //turn params into fetch query format
     const numParams = searchParams.length();
     let formattedParam = `i=${searchParams[0]}`
     for (let i = 0; i < numParams; i++) {
         formattedParam += `%2C${searchParams[i]}`
     }
+    //check if wants alcholic drink
+    const isAlcoholic = document.getElementById("alcohol_box").checked
+    const isNotAlcoholic = document.getElementById("without_alcohol_box").checked
+    if (isAlcoholic && isNotAlcoholic) {
+        //ignore for both otherwise add respective param
+    } else if (isAlcoholic) {
+        formattedParam += "a=Alcoholic"
+    } else if (isNotAlcoholic) {
+        formattedParam += "a=Non-Alcoholic"
+    }
+    //check if wants cocktail or normal drink
+    const isCocktail = document.getElementById("cocktail_box").checked
+    const isNotCocktail = document.getElementById("without_cocktail_box").checked
+    if (isCocktail && isNotCocktail) {
+        //ignore for both otherwise add respective param
+    } else if (isCocktail) {
+        formattedParam += "c=Cocktail"
+    } else if (isNotCocktail) {
+        formattedParam += "c=Ordinary_Drink"
+    }
     //fetch time 
-    let results = []
     fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?${formattedParam}`, {
             "method": "GET",
             "headers": {
